24dead2f505028ce749112a3f6336dbb2a9b567c
RanjanaBhanage <114593855+RanjanaSai@users.noreply.github.com>
2024-02-07 22:28:24 +0800

    Add files via upload

---
+++ reducepolyfill.js
@@ -0,0 +1,32 @@
+if(!Array.prototype.myReduce){
+    Array.prototype.myReduce = function (callback, initial) { 
+      
+            // Variable  result to store result 
+         
+            let result = initial; 
+      
+            for (let i = 0; i < this.length; i++) { 
+                  
+                // If the initialValue exists, we call 
+                // the callback function on the existing 
+                // element and store in result 
+                if (result) { 
+                    result = callback.call( undefined,
+                        result, this[i], i, this); 
+                      
+                    // If initialValue does not exist,  
+                    // we assign result to the 
+                    // current element of the array 
+                } 
+                else { 
+                    result = this[i]; 
+                } 
+            } 
+      
+            // We return result
+            return result; 
+        } 
+    }
+       
+        const arr = [1, 2, 3, 4]; 
+        console.log(arr.myReduce((total, item) => total + item)); 
